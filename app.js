bbdd30cfa79ca05ebc4ebe859838c72e3a34e6a5
dev-rahim <rahimuddin5915@gmail.com>
2023-03-15 10:27:26 +0600

    Implement Search field Validation and Data Not Loading  from API, fix this issue fixed

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
-
 //------------- handle search button-----------
 const searchFood = () => {
-    const searchField = document.getElementById('mealInput'); 
-    const searchData = searchField.value      
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchData}`;
+  const searchField = document.getElementById("mealInput");
+  const searchData = searchField.value;
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchData}`;
+  if (searchField.value) {
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayMealInfo(data))
-}
+      .then((res) => res.json())
+      .then((data) => {
+        displayMealInfo(data.meals);
+      });
+  } else {
+    alert("Please search something");
+  }
+};
 
-const displayMealInfo = mealData => {
-    const mealContainer = document.getElementById('mealCard');
-    mealData.forEach(item => {
-        const foodItemName = document.createElement('div');
-        foodItemName.className = 'meal-items';
-        itemPosition = item.idMeal;
-        const mealInformation = `
+const displayMealInfo = (mealData) => {
+  const mealContainer = document.getElementById("mealCard");
+  console.log(mealData);
+  mealData.forEach((item) => {
+    const foodItemName = document.createElement("div");
+    foodItemName.className = "meal-items";
+    itemPosition = item.idMeal;
+    const mealInformation = `
         <img src ="${item.strMealThumb}">
         <h3>${item.strMeal}</h3>
-        `
-        foodItemName.innerHTML = mealInformation;
-        foodItemName.addEventListener('click', function () {
-            mealIngredientsInfo(item.idMeal);
-        });
-        mealContainer.appendChild(foodItemName);
+        `;
+    foodItemName.innerHTML = mealInformation;
+    foodItemName.addEventListener("click", function () {
+      mealIngredientsInfo(item.idMeal);
     });
-}
+    mealContainer.appendChild(foodItemName);
+  });
+};
 
+//API Call by fetch for meal ingredients
 
-//API Call by fetch for meal ingredients 
-
-const mealIngredientsInfo = mealItemName => { 
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItemName}`;
-fetch(url)
-        .then(response => response.json())
-        .then(data => displayDetails(data))
-}
+const mealIngredientsInfo = (mealItemName) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItemName}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayDetails(data));
+};
 
 //meal ingredients details information
 
-const displayDetails = mealItemDetails => {
-    const mealItemsInformation = document.getElementById('mealItemsInfo');
-    mealItemDetails.forEach(items => {
-        const mealItemsInformations = document.innerHTML('div');
-        mealItemsInformations.className = 'ingredients-info';
-        console.log(items.strMeal);
-        const itemsName = document.createElement('h1');
-        const ingredients = document.createElement('h5');
-        ingredients.innerText = 'Ingredients';
-        itemsName.innerText = items.strMeal;
-        const ul = document.createElement('ul');
-        const imgUrl = document.createElement('img');
-        imgUrl.src = items.strMealThumb;
-        mealItemsInformations.appendChild(imgUrl);
-        const li = `
+const displayDetails = (mealItemDetails) => {
+  const mealItemsInformation = document.getElementById("mealItemsInfo");
+  mealItemDetails.forEach((items) => {
+    const mealItemsInformations = document.innerHTML("div");
+    mealItemsInformations.className = "ingredients-info";
+    console.log(items.strMeal);
+    const itemsName = document.createElement("h1");
+    const ingredients = document.createElement("h5");
+    ingredients.innerText = "Ingredients";
+    itemsName.innerText = items.strMeal;
+    const ul = document.createElement("ul");
+    const imgUrl = document.createElement("img");
+    imgUrl.src = items.strMealThumb;
+    mealItemsInformations.appendChild(imgUrl);
+    const li = `
         
          <li>${items.strIngredient1}</li>
          <li>${items.strIngredient2}</li>
@@ -68,15 +73,11 @@ const displayDetails = mealItemDetails => {
          <li>${items.strIngredient11}</li>
          <li>${items.strIngredient12}</li>
          <li>${items.strIngredient13}</li>
-        `
-        ul.innerHTML = li;
-        mealItemsInformations.appendChild(itemsName);
-        mealItemsInformations.appendChild(ingredients);
-        mealItemsInformations.appendChild(ul);
-        mealItemsInformation.appendChild(mealItemsInformations);
-
-    });
-
-}
-
-
+        `;
+    ul.innerHTML = li;
+    mealItemsInformations.appendChild(itemsName);
+    mealItemsInformations.appendChild(ingredients);
+    mealItemsInformations.appendChild(ul);
+    mealItemsInformation.appendChild(mealItemsInformations);
+  });
+};
